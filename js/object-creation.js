08ff25fea880438b8bd24a2daa1ebec14b9c4f72
Augusto Amaral <augusto.jm.amaral1@gmail.com>
2017-02-13 18:05:16 -0200

    Constructor pattern

---
+++ js/object-creation.js
@@ -0,0 +1,11 @@
+// Formas de criar um objeto vazio
+
+var newObject = {};
+
+var newObject = Object.create( Object.prototype );
+
+var newObject = new Object();
+
+// 4 Formas de atribuir chave e valor a um objeto
+
+// 1. Dot syntax
