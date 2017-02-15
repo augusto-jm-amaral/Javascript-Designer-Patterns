01b41e9f690c3939906443f9008856877303a15e
Augusto Amaral <augusto.jm.amaral1@gmail.com>
2017-02-15 11:20:49 -0200

    Object Creation

--- js/object-creation.js
+++ js/object-creation.js
@@ -9,3 +9,79 @@ var newObject = new Object();
 // 4 Formas de atribuir chave e valor a um objeto
 
 // 1. Dot syntax
+	// Set properties
+	newObject.someKey = "Hello World";
+
+	// Get properties
+	var value = newObject.someKey
+
+// 2. Square bracket syntax
+
+	//Set
+	newObject['someKey'] = 'Hello World';
+
+	//Get
+	var value = newObject['someKey'];
+
+// 3. Object.defineProperty
+
+	//Set
+	Object.defineProperty( newObject, 'someKey', {
+		value: "for more control of the propety's behavior",
+		writable: true,
+		enumerable: true,
+		configurable: true
+	});
+
+	//Deixando um pouco mais simples o setValue acima
+	var defineProperty = function ( obj, key, value) {
+		
+		var config = {
+			value: value,
+			writable: true,
+			enumerable: true,
+			configurable: true
+		};
+
+		Object.defineProperty( obj, key, config);
+	}
+
+	// Use
+	var person = Object.create( Object.prototype );
+	// Populate
+	defineProp( person, 'car', 'Delorean');
+	defineProp( person, 'dateOfBirth', '1981');
+	defineProp( person, 'hasBeard', false);
+
+	console.log(person);
+
+	// 4. Object.defineProperties
+
+	// Set properties
+	Object.defineProperties( newObject, {
+		"someKey": {
+			value: "Hello World"
+			writable: true
+		},
+		"anotherKey": {
+			value: "Foo bar",
+			writable: false
+		}
+	});
+
+
+	// Criando objeto que herda propriedades de outro objeto
+	var driver = Object.create( person );
+
+	// Set
+	defineProp( drive, "topSpeed", "100mph");
+
+	// Get propriedade herdada de person
+	console.log( driver.dateOfBirth ); //--> 1981
+
+
+
+
+
+
+
